e325ccba1244dea293fc527826b6a1b99fdec686
Pawel Oczkowski <pawel.oczko84@gmail.com>
2019-03-01 09:59:35 +0100

    google log-in

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+'GOOGLE_CLIENT_ID': '1075859095490-pc7lt4cjo262sdjaoo8oeecusjr77qmp.apps.googleusercontent.com',
+'GOOGLE_CLIENT_SECRET': 'kdCWMOhdsvnsfxz2-DGKUJ2C',
+'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+
+};
--- server.js
+++ server.js
@@ -1,17 +1,61 @@
 var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./config');
+var googleProfile = {};
 var app = express();
+
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+        clientID: config.GOOGLE_CLIENT_ID,
+        clientSecret: config.GOOGLE_CLIENT_SECRET,
+        callbackURL: config.CALLBACK_URL
+    },
+    function (accessToken, refreshToken, profile, cb) {
+        googleProfile = {
+            id: profile.id,
+            displayName: profile.displayName
+        };
+        cb(null, profile);
+    }
+));
 
 app.get('/', function (req, res) {
-    res.send('Hello world!');
-});
-
-app.get('/auth/google', function (req, res) {
     res.render('auth-google', {
-        url: "http://www.google.com"
+        user: req.user
     });
 });
+
+app.get('/logged', function (req, res) {
+    res.render('logged', {
+        user: googleProfile
+    });
+});
+
+//Passport routes
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }));
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect: '/logged',
+        failureRedirect: '/'
+    }));
 
 app.listen(3000);
 
